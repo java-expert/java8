8eeb8cd84df2183d98421bbebadbcf20723d4ebb
Jagjot Singh <jagjotmca@yahoo.co.in>
2017-04-16 23:07:40 +0530

    lambda sop

---
+++ LambdaSOP_3.java
@@ -0,0 +1,21 @@
+
+public class LambdaSOP_3 {
+	
+	final static String salutation = "Hello! ";
+
+	public static void main(String[] args) {
+
+			   
+			   
+			      GreetingService greetService1 = message -> 
+			      System.out.println(salutation + message);
+			      greetService1.sayMessage("Mahesh");
+			   }
+				
+			   interface GreetingService {
+			      void sayMessage(String message);
+			   }
+		// TODO Auto-generated method stub
+
+
+}
