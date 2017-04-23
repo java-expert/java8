5ea3834d3440fff288955d33be456f07c75a6d45
Jagjot Singh <jagjotmca@yahoo.co.in>
2017-04-23 19:35:02 +0530

    foreach

---
+++ MethodReference_4.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReference_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		 List names = new ArrayList();
+			
+	      names.add("Mahesh");
+	      names.add("Suresh");
+	      names.add("Ramesh");
+	      names.add("Naresh");
+	      names.add("Kalpesh");
+			
+	      names.forEach(System.out::println);
+
+	}
+
+}
